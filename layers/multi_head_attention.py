7d32e50f19d31303c85ce1686dcc0e71dc6f5174
Laurent Thanwerdas <laurent.thanwerdas@gmail.com>
2020-11-15 00:42:56 -0500

    Debug softmax

--- layers/multi_head_attention.py
+++ layers/multi_head_attention.py
@@ -3,7 +3,8 @@ import math
 import string
 
 import numpy as np
-import tensorflow as tf
+from tensorflow.python.keras import backend as K
+from tensorflow.python.framework import dtypes
 
 from tensorflow.python.framework import tensor_shape
 from tensorflow.python.keras import constraints
@@ -98,6 +99,77 @@ def _build_proj_equation(free_dims, bound_dims, output_dims):
 def _get_output_shape(output_rank, known_last_dims):
   return [None] * (output_rank - len(known_last_dims)) + list(known_last_dims)
 
+def _large_compatible_negative(tensor_type):
+  """Large negative number as Tensor.
+  This function is necessary because the standard value for epsilon
+  in this module (-1e9) cannot be represented using tf.float16
+  Args:
+    tensor_type: a dtype to determine the type.
+  Returns:
+    a large negative number.
+  """
+  if tensor_type == dtypes.float16:
+    return dtypes.float16.min
+  return -1e9
+
+class Softmax(Layer):
+  """Softmax activation function.
+  Example without mask:
+  >>> inp = np.asarray([1., 2., 1.])
+  >>> layer = tf.keras.layers.Softmax()
+  >>> layer(inp).numpy()
+  array([0.21194157, 0.5761169 , 0.21194157], dtype=float32)
+  >>> mask = np.asarray([True, False, True], dtype=bool)
+  >>> layer(inp, mask).numpy()
+  array([0.5, 0. , 0.5], dtype=float32)
+  Input shape:
+    Arbitrary. Use the keyword argument `input_shape`
+    (tuple of integers, does not include the samples axis)
+    when using this layer as the first layer in a model.
+  Output shape:
+    Same shape as the input.
+  Arguments:
+    axis: Integer, or list of Integers, axis along which the softmax
+      normalization is applied.
+  Call arguments:
+    inputs: The inputs, or logits to the softmax layer.
+    mask: A boolean mask of the same shape as `inputs`. Defaults to `None`.
+  Returns:
+    softmaxed output with the same shape as `inputs`.
+  """
+
+  def __init__(self, axis=-1, **kwargs):
+    super(Softmax, self).__init__(**kwargs)
+    self.supports_masking = True
+    self.axis = axis
+
+  def call(self, inputs, mask=None):
+    if mask is not None:
+      # Since attention_mask is 1.0 for positions we want to attend and 0.0 for
+      # masked positions, this operation will create a tensor which is 0.0 for
+      # positions we want to attend and -1e.9 for masked positions.
+      adder = (1.0 - math_ops.cast(mask, inputs.dtype)) * (
+          _large_compatible_negative(inputs.dtype))
+
+      # Since we are adding it to the raw scores before the softmax, this is
+      # effectively the same as removing these entirely.
+      inputs += adder
+    if isinstance(self.axis, (tuple, list)):
+      if len(self.axis) > 1:
+        return math_ops.exp(inputs - math_ops.reduce_logsumexp(
+            inputs, axis=self.axis, keepdims=True))
+      else:
+        return K.softmax(inputs, axis=self.axis[0])
+    return K.softmax(inputs, axis=self.axis)
+
+  def get_config(self):
+    config = {'axis': self.axis}
+    base_config = super(Softmax, self).get_config()
+    return dict(list(base_config.items()) + list(config.items()))
+
+  @tf_utils.shape_type_conversion
+  def compute_output_shape(self, input_shape):
+    return input_shape
 
 class MultiHeadAttention(Layer):
   """MultiHeadAttention layer.
@@ -345,7 +417,7 @@ class MultiHeadAttention(Layer):
         _build_attention_equation(rank, attn_axes=self._attention_axes))
     norm_axes = tuple(
         range(attn_scores_rank - len(self._attention_axes), attn_scores_rank))
-    self._softmax = tf.keras.layers.Softmax(axis=norm_axes)
+    self._softmax = Softmax(axis=norm_axes)
     self._dropout_layer = core.Dropout(rate=self._dropout)
 
   def _masked_softmax(self, attention_scores, attention_mask=None):
